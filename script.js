a72de0414ebcdf26d4f064478ddd3aeed77373b4
Nishant Gupta <gupta.nishant08042004@gmail.com>
2025-04-22 09:57:49 +0530

    add toggle images

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const burgerImages = document.querySelectorAll('.burger img');
+const toggleImages = document.querySelectorAll('.burger-toggle img');
+let currentIndex = 0;
+
+toggleImages[currentIndex].classList.add('active');
+
+burgerImages.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        toggleImages.forEach(toggleImg => toggleImg.classList.remove('active'));
+        toggleImages[currentIndex].classList.add('active');
+        toggleImages[currentIndex].src = burgerImages[index].src;
+        currentIndex = (currentIndex + 1) % toggleImages.length;
+    });
+});
+
+
 function createStars() {
     const starsContainer = document.getElementById('stars');
     const windowWidth = window.innerWidth;
